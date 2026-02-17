293908b0e3ca5dd60343b30ac6983a817bbddbd0
romanesl72 <romanesl72@gmail.com>
2026-02-17 11:49:31 +0100

    Ajout de documentation

--- lib/chargerMatrice.h
+++ lib/chargerMatrice.h
@@ -1,23 +1,25 @@
-/**
- * @file chargerMatrice.h
- * @brief Fonctions pour charger une matrice depuis un fichier.
- */
-
 #ifndef _CHARGER_MATRICE_H_
 #define _CHARGER_MATRICE_H_
 
-
 #include "types.h"
 
+/** 
+ * @file chargerMatrice.h
+ * @brief Fonctions pour charger une matrice depuis un fichier.
+ * @author Romane Saint_Léger
+ * @date Crée le 07/02/2026
+ */
 
 /**
+ * @fn void chargerMatriceDepuisFichier(const char* nomFichier, int matrice[MAT_H][MAT_L]);
  * @brief Charge une matrice depuis un fichier.
+ * @author Romane Saint-Léger
+ * @date Crée le 07/02/2026
  * @param nomFichier Le nom du fichier à lire.
  * @param matrice La matrice à remplir avec les données du fichier.
- * @return int Retourne 1 si le chargement a réussi, 0 sinon.
  */
-void chargerMatriceDepuisFichier(const char* nomFichier, int matrice[MAT_H][MAT_L]);
 
+void chargerMatriceDepuisFichier(const char* nomFichier, int matrice[MAT_H][MAT_L]);
 
 
 #endif
--- lib/collision_decor.h
+++ lib/collision_decor.h
@@ -1,20 +1,26 @@
-/**
- * @file collision_decor.h
- * @brief Fonctions de détection de collision avec la matrice.
- */
-
 #ifndef _COLLISION_DECOR_H_
 #define _COLLISION_DECOR_H_
 
 #include "types.h"
 
+ /** 
+ * @file collision_decor.h
+ * @brief Fonctions de détection de collision du dino avec la matrice.
+ * @author Romane Saint_Léger
+ * @date Crée le 08/02/2026
+ */
+
 /**
+ * @fn int collision_decor(int tab_res[4], t_dino dino, int matrice[MAT_H][MAT_L]);
  * @brief Vérifie les collisions autour du dinosaure.
+ * @author Romane Saint-Léger
+ * @date Crée le 08/02/2026
  * @param tab_res Tableau de 4 entiers [SUD, OUEST, EST, NORD] à remplir (1 si collision).
  * @param dino La structure du dinosaure testé.
  * @param matrice La matrice représentant le terrain.
  * @return int Retourne 1 si au moins une collision (terre) est détectée, 0 sinon.
  */
+
 int collision_decor(int tab_res[4], t_dino dino, int matrice[MAT_H][MAT_L]);
 
 #endif
