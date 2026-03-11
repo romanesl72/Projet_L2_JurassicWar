93fd35319d35b553ff057d24273d244fe0901cda
hsergent4 <hannah.sergent@laposte.net>
2026-03-11 11:44:36 +0100

    création d'une fonction d'initialisation et de destruction de matrice

--- lib/chargerMatrice.h
+++ lib/chargerMatrice.h
@@ -6,7 +6,7 @@
 /** 
  * @file chargerMatrice.h
  * @brief Fonctions pour charger une matrice depuis un fichier.
- * @author Romane Saint_Léger
+ * @author Romane Saint_Léger Hannah Sergent
  * @date Crée le 07/02/2026
  */
 
@@ -20,6 +20,29 @@
  */
 
 void chargerMatriceDepuisFichier(const char* nomFichier, int matrice[MAT_H][MAT_L]);
+
+/**
+ * @fn int initialiserMatrice(t_case (**matriceTerrain)[LARGEUR_TERRAIN]);
+ * @brief la fonction initialise la matrice du terrain
+ * @author Hannah Sergent
+ * @date Crée le 11/03/2026
+ * @version 1.0
+ * @param matriceTerrain matrice qui contient les informations sur le terrain
+ * @return 1 si tout s'est bien passé et 0 sinon
+ */
+
+int initialiserMatrice(t_case (**matriceTerrain)[LARGEUR_TERRAIN]);
+
+/**
+ * @fn void detruireMatrice(t_case (**matriceTerrain)[LARGEUR_TERRAIN]);
+ * @brief la fonction détruit la matrice du terrain
+ * @author Hannah Sergent
+ * @date Crée le 11/03/2026
+ * @version 1.0
+ * @param matriceTerrain La matrice à détruire
+ */
+
+void detruireMatrice(t_case (**matriceTerrain)[LARGEUR_TERRAIN]);
 
 
 #endif
