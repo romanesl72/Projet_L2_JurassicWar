c9ad687bda0d3e64666db11be7e61964ee99e755
romanesl72 <romanesl72@gmail.com>
2026-03-11 10:35:42 +0100

    Rajout de documentation

--- lib/deplacement.h
+++ lib/deplacement.h
@@ -9,14 +9,111 @@
 #include "collision_decor.h"
 #include "fonctionsVerification.h"
 
+/** 
+ * @file deplacement.h
+ * @brief 
+ * @author Solène Orieux
+ * @date 
+ */
+
+
+/**
+ * @def VITESSE_BASE
+ * @brief
+*/
+
 #define VITESSE_BASE 0.05
-#define GRAVITE 0.6
-#define FORCE_SAUT -10.0
+
+/**
+ * @def GRAVITE
+ * @brief
+*/
+
+#define GRAVITE 0.8
+
+/**
+ * @def FORCE_SAUT
+ * @brief
+*/
+
+#define FORCE_SAUT -12.0
+
+/**
+ * @fn void supprimer_matrice_dino(t_dino *dino, int matrice[MAT_H][MAT_L]);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param matrice
+ */
 
 void supprimer_matrice_dino(t_dino *dino, int matrice[MAT_H][MAT_L]);
+
+/**
+ * @fn int chercherNouveauIndiceNuage(t_dino *dino, t_coordonnee *nuage, int *nb_pts, int matrice[MAT_H][MAT_L]);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param nuage
+ * @param nb_pts
+ * @param matrice
+ */
 int chercherNouveauIndiceNuage(t_dino *dino, t_coordonnee *nuage, int *nb_pts, int matrice[MAT_H][MAT_L]);
+
+/**
+ * @fn int horsNuage(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L], int mvt);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param nuage
+ * @param nb_pts
+ * @param matrice
+ * @param mvt
+ */
+
 int horsNuage(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L], int mvt);
+
+/**
+ * @fn void gauche(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param nuage
+ * @param nb_pts
+ * @param matrice
+ * @param state
+ */
+
 void gauche(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
+
+/**
+ * @fn void droite(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param nuage
+ * @param nb_pts
+ * @param matrice
+ * @param state
+ */
+
 void droite(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
+
+/**
+ * @fn void saut(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
+ * @author Solène Orieux
+ * @date 
+ * @brief
+ * @param dino
+ * @param nuage
+ * @param nb_pts
+ * @param matrice
+ * @param state
+ */
+
 void saut(t_dino *dino, t_coordonnee *nuage, int nb_pts, int matrice[MAT_H][MAT_L],const Uint8 *state);
 #endif
