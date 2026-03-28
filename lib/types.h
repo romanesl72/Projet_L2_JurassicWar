cdc113d749c6c6c16c96ade5dcc152731c9ee19a
hsergent4 <hannah.sergent@laposte.net>
2026-03-28 18:22:54 +0100

    Modification de types.h + ajout de commentaires doxygen par la même occasion

--- lib/types.h
+++ lib/types.h
@@ -5,97 +5,144 @@
 
 /**
  * @file types.h
- * @brief Définition des structures de données pour Jurassic War.
+ * @brief Définition des structures de données pour JurassicWar.
  * @author Solène Orieux Romane Saint-Léger Hannah Sergent
+ * @date Crée le 27/01/2026
+ * @version 2.0
  */
 
-/* Variables globales communes à la matrice et aux fenetres à utiliser*/
+/**
+ * @def LARGEUR_TERRAIN
+ * @brief Largeur du terrain, de la matrice et de la fenêtre.
+*/
+
 #define LARGEUR_TERRAIN 1300
+
+/**
+ * @def HAUTEUR_TERRAIN
+ * @brief Hauteur du terrain, de la matrice et de la fenêtre.
+*/
+
 #define HAUTEUR_TERRAIN 700
 
 #define MAT_H 700  
 #define MAT_L 1300
 #define VIDE 0
+
+/**
+ * @def TAILLE_DINO
+ * @brief Taille de l'image dinosaure.
+*/
+
 #define TAILLE_DINO 30
+
+/**
+ * @def NOMBRE_DINOS
+ * @brief Nombre de dinosaures dans le jeu.
+*/
 
 #define NOMBRE_DINOS 6
 
 /**
  * @enum t_case
- * @brief Représentation de chaque élément sur la matrice 
+ * @brief Représentation de chaque élément sur la matrice.
+ * @author Solène Orieux Romane Saint-Léger Hannah Sergent
+ * @date Crée le 27/01/2026
+ * @version 1.0
 */
 
-typedef enum {EAU=-1, AIR, TERRE, D1, D2,D3,D4,D5,D6} t_case; 
+typedef enum {
+    EAU=-1, /**< Valeur représentant l'eau */
+    AIR,    /**< Valeur représentant l'air */
+    TERRE,  /**< Valeur représentant la terre */
+    D1,     /**< Dinosaure numéro 1 */
+    D2,     /**< Dinosaure numéro 2 */
+    D3,     /**< Dinosaure numéro 3 */
+    D4,     /**< Dinosaure numéro 4 */
+    D5,     /**< Dinosaure numéro 5 */
+    D6      /**< Dinosaure numéro 6 */
+} t_case; 
 
 /**
  * @enum t_nom_arme
- * @brief Toutes les armes utilisés hormis la bombe
+ * @brief Toutes les armes utilisées hormis la bombe.
  * @author Romane Saint-Léger
  * @date Crée le 07/03/2026
  * @version 1.3
 */
+
 typedef enum {ARC, ARBALETE, FUSIL, REVOLVER } t_nom_arme;
 
-typedef enum {GAUCHE, DROITE} t_cote;
+/**
+ * @enum t_cote
+ * @brief Indique la direction dans laquelle pointe les dinosaures ou les armes.
+ * @author Hannah Sergent
+ * @date Crée le 24/03/2026
+ * @version 1.0
+*/
 
+typedef enum {
+    GAUCHE, /**< Direction gauche */
+    DROITE  /**< Direction droite */
+} t_cote;
 
 /**
  * @struct t_coordonnee
- * @brief Structure de coordonnées entières classiques (pixels)
+ * @brief Structure de coordonnées entières classiques (pixels).
  * @author Solène Orieux Romane Saint-Léger Hannah Sergent
  * @date Crée le 27/01/2026
  * @version 1.0
 */
 
 typedef struct {
-	int x; /**< Position Horizontale */
-	int y; /**< Position Verticale */
+	int x; /**< Position horizontale */
+	int y; /**< Position verticale */
 } t_coordonnee;
 
 /**
  * @struct t_coordonnee_calcul
- * @brief Structure qui contient des coordonnées flottantes pour ne pas perdre de précision dans les calculs
+ * @brief Structure qui contient des coordonnées flottantes pour ne pas perdre de précision dans les calculs.
  * @author Hannah Sergent
  * @date Crée le 2/03/2026
  * @version 1.0
  */
 
 typedef struct {
-	float x;
-	float y;
+	float x; /**< Position horizontale flottante */
+	float y; /**< Position verticale flottante */
 } t_coordonnee_calcul;
 
 /**
  * @struct t_deplacement
- * @brief Structure regroupant toutes les informations de déplacement d'un dinosaure
+ * @brief Structure regroupant toutes les informations de déplacement d'un dinosaure.
  * @author Solène Orieux
  * @date Crée le 14/03/2026
  * @version 1.0
 */
 
 typedef struct {
-    int sautBooleen;        /**< Vaut 1 si le dinosaure est en cours de saut */
-    int id_nuage_temp;      /**< prochain nuage */
-    int indice_nuage_temp;  /**< Position temporaire dans le prochain nuage */
-    float indice_reel;      /**< Pour le déplacement */
-    int tab_res[4];         /**< pour les colisions */
-    float v_y;              /**< pour les sauts */
-    int hors_nuage;         /**<  */
-    int wait;               /**< Petite pause pour ne pas sauter deux fois d'un coup*/
-    int taille_nuage[5];    /**< donne la taille de chaque nuage */
-    t_coordonnee coteProche[5];      /**< donne le premier et le dernier x de chaque nuage */
+    int sautBooleen;            /**< Vaut 1 si le dinosaure est en cours de saut */
+    int id_nuage_temp;          /**< prochain nuage */
+    int indice_nuage_temp;      /**< Position temporaire dans le prochain nuage */
+    float indice_reel;          /**< Pour le déplacement */
+    int tab_res[4];             /**< pour les colisions */
+    float v_y;                  /**< pour les sauts */
+    int hors_nuage;             /**<  */
+    int wait;                   /**< Petite pause pour ne pas sauter deux fois d'un coup*/
+    int taille_nuage[5];        /**< donne la taille de chaque nuage */
+    t_coordonnee coteProche[5]; /**< donne le premier et le dernier x de chaque nuage */
 } t_deplacement;
 
 /**
  * @struct t_dino
- * @brief Structure regroupant toutes les informations d'un dinosaure
+ * @brief Structure regroupant toutes les informations d'un dinosaure.
  * @author Solène Orieux Romane Saint-Léger Hannah Sergent
  * @date Crée le 27/01/2026
  * @version 3.0
 */
 
 typedef struct {
-    t_case d;            /**< Numéro du dinosaure de 0 à 5 */
+    t_case d;               /**< Numéro du dinosaure de 2 à 7 */
     t_coordonnee pos;       /**< Position actuelle */
     int id_nuage;           /**< Numéro du nuage */
     int indice_nuage;       /**< Position dans le nuage */
@@ -108,37 +155,38 @@ typedef struct {
 
 /**
  * @struct t_joueur
- * @brief Structure représentant un joueur et son équipe de dinosaure
+ * @brief Structure représentant un joueur et son équipe de dinosaure.
  * @author Solène Orieux Romane Saint-Léger Hannah Sergent
  * @date Crée le 07/02/2026
- * @version 1.0
+ * @version 2.0
 */
 
 typedef struct {
-	t_dino * tab;   /**< Tableau de dinosaure */
-    SDL_Texture **texDinos; /**< Tableau contenant les images de dinosaure */
-    SDL_Texture **texDinosInv; /**< Tableau contenant les images de dinosaure dans le sens inverse */
-	int n;          /**< Taille du tableau*/ 
+	t_dino *tab;                /**< Tableau de dinosaures */
+    SDL_Texture **texDinos;     /**< Tableau contenant les images des dinosaures */
+    SDL_Texture **texDinosInv;  /**< Tableau contenant les images des dinosaures dans le sens inverse */
+    t_cote *tabCote;            /**< Tableau contenant la direction du dinosaure à l'instant t */
+	int n;                      /**< Taille des tableaux*/ 
 } t_joueur;
 
 /**
  * @struct t_tour
- * @brief Structure représentant la gestion des tours
+ * @brief Structure représentant la gestion des tours.
  * @author Hannah Sergent
  * @date Crée le 24/03∕2026
  * @version 1.0
 */
 
 typedef struct {
-    int numeroTour;
-    int equipeCourante;
-    t_case dinoCourant;
-    t_case dinoPrecedent;
+    int numeroTour;         /**< Numéro du tour en train de se produire */
+    int equipeCourante;     /**< Numéro de l'équipe à qui c'est le tour de jouer */
+    t_case dinoCourant;     /**< Numéro du dinosaure qui peut d'agir */
+    t_case dinoPrecedent;   /**< Numéro du dinosaure qui a fait une action au tour précédent */
 } t_tour;
 
 /**
  * @struct t_zone_depart
- * @brief  Zone où un dinosaure peut apparaitre
+ * @brief  Zone où un dinosaure peut apparaitre.
  * @author Romane Saint-Léger
  * @date Crée le 17/02/2026
  * @version 1.2
@@ -154,7 +202,7 @@ typedef struct {
 
 /**
  * @struct t_catalogue_zones
- * @brief Structures qui contient les zones des différentes équipes
+ * @brief Structures qui contiennent les zones des différentes équipes.
  * @author Romane Saint-Léger
  * @date Crée le 17/02/2026
  * @version 1.0
@@ -168,49 +216,49 @@ typedef struct {
 
 /**
  * @struct t_vect
- * @brief 
+ * @brief Une structure représentant un vecteur de R carré.
  * @author Solène Orieux Hannah Sergent
  * @date Crée le 5/02/2026
  * @version 2.3
 */
 
 typedef struct {
-	float u;    /**< */
-	float v;    /**< */
+	float u;    /**< Première coordonnée du vecteur */
+	float v;    /**< Deuxième coordonnée du vecteur */
 } t_vect;
 
 /**
  * @struct t_bombe
- * @brief Structure qui contient les coordonnées et le rayon d'une bombe
+ * @brief Structure qui contient les coordonnées et le rayon d'une bombe.
  * @author Hannah Sergent
  * @date Crée le 17/02/2026
  * @version 2.0
  */
 
 typedef struct {
-    t_coordonnee_calcul coor;   /**< */
-    int rayon;                  /**< */
+    t_coordonnee_calcul coor;   /**< Les coordonnées flottantes de la bombe */
+    int rayon;                  /**< Le rayon de la bombe */
 } t_bombe;
 
 /**
  * @struct t_arme
- * @brief Caractéristiques de chaque arme
+ * @brief Caractéristiques de chaque arme.
  * @author Romane Saint-Léger
  * @date Crée le 7/03/2026
  * @version 1.3
 */
 
 typedef struct {
-    t_nom_arme nom;             /**< Nom de l'arme utilisé*/
-    int degats;                 /**< Degats effectués par l'arme*/
-    float poids_projectile;     /**< Poids de l'arme, influence sa courbe (gravité)*/
-    float puissance_propulsion; /**< Vitesse de base*/
-    float vitesse_max;          /**< Vitesse Maximale*/
+    t_nom_arme nom;             /**< Nom de l'arme utilisée */
+    int degats;                 /**< Dégats effectués par l'arme */
+    float poids_projectile;     /**< Poids de l'arme, influence sa courbe (gravité) */
+    float puissance_propulsion; /**< Vitesse de base */
+    float vitesse_max;          /**< Vitesse Maximale */
 }t_arme;
 
 /**
  * @struct t_tir
- * @brief Données nécessaire pour calculer un tir
+ * @brief Données nécessaires pour calculer un tir.
  * @author Romane Saint-Léger
  * @date Crée le 7/03/2026
  * @version 1.0
@@ -225,7 +273,7 @@ typedef struct {
 
 /**
  * @struct t_element_file
- * @brief élement d'une file
+ * @brief élement d'une file.
  * @author Solène Orieux
  * @date Crée le 25/03/2026
  * @version 1.0
